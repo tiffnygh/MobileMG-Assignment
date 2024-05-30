befba93ccaa72337b95fa307a4673ab3df12aa79
TerrenceC1211 <70148874+TerrenceC1211@users.noreply.github.com>
2024-05-31 02:31:01 +0800

    add dotween package

--- Assets/Scripts/Manager/ShopManager.cs
+++ Assets/Scripts/Manager/ShopManager.cs
@@ -14,7 +14,6 @@ public class ShopManager : Singleton<ShopManager>
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
